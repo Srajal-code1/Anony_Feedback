bc1dee8d228a08122b3e439d86322aaf8b9d0520
Srajal-code1 <srajalasthana78@gmail.com>
2026-07-18 23:54:21 +0530

    updated context and auth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-function page() {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>
-      HI lol
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default page
---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,83 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+type AuthUser = {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+};
+
+export const authOptions : NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id : "credentials",
+            name : "Credentials",
+            credentials : {
+                email : {label: "Email", type: "text"},
+                password : {label: "Password", type: "password"}
+            },
+            async authorize(credentials: any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            {email: credentials.identifier},
+                            {username: credentials.identifier}
+                        ]
+                    })
+                    if(!user) {
+                        throw new Error('No user found with this email')
+                    }
+
+                    if(!user.isVerified) {
+                        throw new Error('Please verify your account before login')
+                    }
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+                        if(isPasswordCorrect) {
+                            return user
+                        }else {
+                            throw new Error('Incorrect Password')
+                        }
+                } catch (err : any) {
+                    throw new Error(err)
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                const authUser = user as AuthUser;
+                token._id = authUser._id?.toString();
+                token.isVerified = authUser.isVerified;
+                token.isAcceptingMessages = authUser.isAcceptingMessages;
+                token.username = authUser.username;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user = {
+                    ...session.user,
+                    _id: token._id,
+                    isVerified: token.isVerified,
+                    isAcceptingMessages: token.isAcceptingMessages,
+                    username: token.username,
+                };
+            }
+            return session; 
+        },  
+    },
+    pages: {
+        signIn: '/sign-in',
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
+}
